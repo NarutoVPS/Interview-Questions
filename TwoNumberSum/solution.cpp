4cf8b935071c85c1a678a80a3605957fe6b20ebc
Naruto <singhvedprakash075@gmail.com>
2020-05-12 00:10:16 +0530

    added comments

--- TwoNumberSum/solution.cpp
+++ TwoNumberSum/solution.cpp
@@ -18,6 +18,8 @@ pair<int, int> solve(const vector<int> &arr, const int &target) {
 }
 */
 
+
+//                Better Approach--> O(n) time | O(n) space
 pair<int, int> solve(const vector<int> &arr, const int &target) {
     unordered_map<int, int> available;
     int currentNum{};
@@ -35,6 +37,6 @@ int main() {
     vector<int> arr {4, 7, -4, 2, 2, 2, 3, -5, -3, 9, -4, 9, -7, 7, -1, 9, 9, 4, 1, -4, -2, 3, -3, -5, 4, -7, 7, 9, -4, 4, -8};
     auto ans = solve(arr, -3);
     cout<<ans.first<<" "<<ans.second<<endl;
-    
+
     return 0;
 }
