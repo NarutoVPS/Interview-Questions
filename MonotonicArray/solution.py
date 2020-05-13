c3f76e648257cacd856c8c78cc9ada2e0d804b86
Naruto <singhvedprakash075@gmail.com>
2020-05-13 20:35:32 +0530

    Python solution of MonotonicArray

---
+++ MonotonicArray/solution.py
@@ -0,0 +1,45 @@
+'''
+    An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+    An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+    Return true if and only if the given array A is monotonic.
+
+        Example 1:
+            Input: [1,2,2,3]
+            Output: true
+
+        Example 2:
+            Input: [6,5,4,4]
+            Output: true
+
+        Example 3:
+            Input: [1,3,2]
+            Output: false
+
+        Example 4:
+            Input: [1,2,4,5]
+            Output: true
+
+        Example 5:
+            Input: [1,1,1]
+            Output: true
+'''
+
+# O(n) time | O(1) space
+def solve(arr):
+    if len(arr) < 3:
+        return True
+    direction = arr[1] - arr[0]
+    for i in range(2, len(arr)):
+        if direction == 0:
+            direction = arr[i] - arr[i-1]
+            continue
+        if direction > 0:
+            if arr[i] - arr[i-1] < 0:
+                return False
+        elif arr[i] - arr[i-1] > 0:
+            return False
+    return True
+
+print(solve([1,1,1]))
