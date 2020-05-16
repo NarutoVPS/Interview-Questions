df4536c755cdda9c1a965c67dcbfaae3c3a8b5d0
Naruto <singhvedprakash075@gmail.com>
2020-05-16 22:43:53 +0530

    added comments plus minor fixes

--- LongestMountain/solution.py
+++ LongestMountain/solution.py
@@ -18,25 +18,32 @@
 
 # O(n) time | O(1) space
 def solve(arr):
+    # if the length of arr is less than 3, no peak/mountain exists
     if len(arr) < 3:
         return 0
 
-    current =  1
-    maxPeak = 0
+    current, maxPeak =  1, 0
+
+    # traverse through the array
     while current < len(arr)-1:
+        # check if current element is a peak/mountain
         if arr[current] - arr[current-1] > 0 and arr[current] - arr[current+1] > 0:
             count, i = 1, current
+            # count the elements that are decreasing at left side of the peak/mountain
             while i > 0 and arr[i] - arr[i-1] > 0:
                 count += 1
                 i -= 1
 
             i = current
+            # count the elements that are decreasing at right side of the peak/mountain
             while i < len(arr)-1 and arr[i] - arr[i+1] > 0:
                 count += 1
                 i += 1 
 
+            # continue traversing the array from the end of the current peak/mountain
             current = i
 
+            # if the length of current peak/mountain is greater than maxPeak, update maxPeak
             maxPeak = max(maxPeak, count)
 
         else:
