05d352c65422337f1755273bdc76efddf424feca
Naruto <singhvedprakash075@gmail.com>
2020-05-17 19:33:47 +0530

    aadded find method

--- BST_Construction/solution.py
+++ BST_Construction/solution.py
@@ -20,3 +20,15 @@ class BST:
                     break
                 else:
                     currentNode = currentNode.left
+
+    def find(self, data):
+        currentNode = self
+        while currentNode != None:
+            if currentNode.data == data:
+                return True
+            if data < currentNode.data:
+                currentNode = currentNode.left
+            else:
+                currentNode = currentNode.right
+        
+        return False
