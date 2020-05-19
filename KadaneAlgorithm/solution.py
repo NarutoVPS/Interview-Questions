dd65c128257422f97e1d6dd183d56be81e46c8b3
Naruto <singhvedprakash075@gmail.com>
2020-05-20 03:31:52 +0530

    added question

--- KadaneAlgorithm/solution.py
+++ KadaneAlgorithm/solution.py
@@ -1,6 +1,15 @@
+'''
+    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+    Example:
+        Input: [-2,1,-3,4,-1,2,1,-5,4],
+        Output: 6
+        Explanation: [4,-1,2,1] has the largest sum = 6.
+'''
+
 def solve(arr):
     if not len(arr):
-        return 0;
+        return 0
     if len(arr) == 1:
         return arr[0]
 
