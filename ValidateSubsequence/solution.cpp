b926dbcf04cfd6b9e40222f89f7181a12eb81f7f
Naruto <singhvedprakash075@gmail.com>
2020-06-04 17:22:07 +0530

    added question & complexity

--- ValidateSubsequence/solution.cpp
+++ ValidateSubsequence/solution.cpp
@@ -1,7 +1,14 @@
+/*
+Given two arrays, check if the 2nd array is a valid subsequence of the 1st array.
+To be a valid subsequence all the elements in 2nd array must be present in 1st array.
+The elements of the 2nd array must preserve its relative positions.
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
+// O(n) time | O(1) space
 bool solve(const vector<int> &mainArray, const vector<int> &subArray){
 	int mainArrayIndex{}, subArrayIndex{};
 	while(mainArrayIndex < mainArray.size() && subArrayIndex < subArray.size()) {
--- ValidateSubsequence/solution.py
+++ ValidateSubsequence/solution.py
@@ -1,3 +1,10 @@
+'''
+	Given two arrays, check if the 2nd array is a valid subsequence of the 1st array.
+	To be a valid subsequence all the elements in 2nd array must be present in 1st array.
+	The elements of the 2nd array must preserve its relative positions.
+'''
+
+# O(n) time | O(1) space
 def solve(mainArr, subArr):
 	mainArrIndex = subArrIndex = 0
 
