763d70efed5eb577df77ad3fcca0f47fe9acecd8
Naruto <singhvedprakash075@gmail.com>
2020-07-20 17:11:41 +0530

    added cpp solution

---
+++ FibonacciNumber/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// O(2^n) Time | O(n) Space
+long long solve(int n) {
+    if (n < 1)
+        return -1;
+    if (n == 1 || n == 2)
+        return 1;
+    return solve(n-1) + solve(n-2);   
+}
+
+// O(n) Time | O(n) Space
+long long solve2(int n) {
+    unordered_map<int, long long> cache{};
+    cache[1] = 1;
+    cache[2] = 1;
+
+    if (n < 1)
+        return -1;
+    if (cache.find(n) != cache.end())
+        return cache[n];
+    else {
+        cache[n] = solve2(n-1) + solve2(n-2);
+        return cache[n];
+    }
+}
