abf85b28b664c01804c57f47bb263abf565cd7c8
Naruto <singhvedprakash075@gmail.com>
2020-07-21 21:52:01 +0530

    added question

--- ProductSum/solution.py
+++ ProductSum/solution.py
@@ -1,3 +1,11 @@
+'''
+    Given an array cantaining sub-array(s) upto say depth 'd', find the sum of all the elements of the array such that the sum of all elements of each sub-array is multiplied with its respective depth.
+
+    Example :-
+        Given array -> [1, 2, 3, [4, 5]]
+        Product Sum = 24
+'''
+
 # O(n) Time | O(d) Space (d -> depth of sub-array)
 def solve(arr, multiplier = 1):
     sum = 0
